5f16ff31fba691e39df6cb8897f6f06672249cc9
xuech <xuech_love@126.com>
2018-01-23 10:31:01 +0800

    Initial Commit

---
+++ LoadingAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingAnimation
+//
+//  Created by xuech on 2018/1/23.
+//  Copyright © 2018年 xuech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
